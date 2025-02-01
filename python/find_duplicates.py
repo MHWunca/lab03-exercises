ce912925360555318a50d15b333d4038f8839eb7
Xtrouble <38140929+Xtrouble@users.noreply.github.com>
2025-01-31 19:40:51 -0500

    Unify python functions

--- python/find_duplicates.py
+++ python/find_duplicates.py
@@ -1,4 +1,4 @@
-def find_duplicates_nested_loop(l: list) -> list:
+def find_duplicates_nested_loop(l: list, all = False) -> list:
     s = l.copy()
     d = []
     for i in range(len(s)-1):
@@ -8,21 +8,10 @@ def find_duplicates_nested_loop(l: list) -> list:
             if (s[i]==s[j]):
                 if a:
                     d.append(s.pop(j))
-                    a = False
+                    if all:
+                        a = False
                 else:
                     s.pop(j)
-            else:
-                j += 1
-    return d
-
-def find_all_duplicates_nested_loop(l: list) -> list:
-    s = l.copy()
-    d = []
-    for i in range(len(s)-1):
-        j = i+1
-        while j<len(s):
-            if (s[i]==s[j]):
-                d.append(s.pop(j))
             else:
                 j += 1
     return d
@@ -37,9 +26,9 @@ if __name__ == "__main__":
     print("Sample 2:", find_duplicates_nested_loop(sample2))
     print("Sample 3:", find_duplicates_nested_loop(sample3))
     print("Sample 4:", find_duplicates_nested_loop(sample4))
-    '''
-    print("Sample 1:", find_all_duplicates_nested_loop(sample1))
-    print("Sample 2:", find_all_duplicates_nested_loop(sample2))
-    print("Sample 3:", find_all_duplicates_nested_loop(sample3))
-    print("Sample 4:", find_all_duplicates_nested_loop(sample4))
-    '''
+    #'''
+    print("Sample 1:", find_duplicates_nested_loop(sample1, all=True))
+    print("Sample 2:", find_duplicates_nested_loop(sample2, all=True))
+    print("Sample 3:", find_duplicates_nested_loop(sample3, all=True))
+    print("Sample 4:", find_duplicates_nested_loop(sample4, all=True))
+    #'''
